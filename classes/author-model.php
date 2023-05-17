b01dd41c032f011045cde804b562d357dacf8f75
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:19:25 +0200

    author model

--- classes/author-model.php
+++ classes/author-model.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once 'db.php';
+
+class AuthorModel extends DB {
+
+    protected $table = 'authors';
+
+    public function getAllAuthors() {
+        return $this->getAll($this->table);
+    }
+}
