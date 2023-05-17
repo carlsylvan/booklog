dec1a3ae8171e1563e5926bc96e47ce8a3cea60f
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:23:19 +0200

    book view

--- classes/book-view.php
+++ classes/book-view.php
@@ -1,1 +1,12 @@
 <?php
+
+class BookView {
+
+    public function renderAllBooks(array $books): void {
+        echo "<ul>";
+        foreach ($books as $book) {
+            echo "<li>{$book['title']} ({$book['year']})</li>";
+        }
+        echo "</ul>";
+    }
+}
