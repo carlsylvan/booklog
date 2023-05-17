bfcf42c5713733c4a3a33120b5418f09833578bc
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:57:16 +0200

    connect

--- partials/connect.php
+++ partials/connect.php
@@ -7,7 +7,7 @@ function connect ($host, $db, $user, $password) {
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
 
     try {
-        $options = [PDO::ATTR_ERRMODE] => PDO::ERRMODE_EXCEPTION;
+        $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
         return new PDO($dsn, $user, $password, $options);
     } catch (PDOException $e) {
