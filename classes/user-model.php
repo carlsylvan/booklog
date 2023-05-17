fbc2b3e0bd239c344a938bcc48ae0c58ac82e852
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:25:42 +0200

    user model

--- classes/user-model.php
+++ classes/user-model.php
@@ -1,1 +1,12 @@
 <?php
+
+require_once 'db.php';
+
+class UserModel extends DB {
+
+    protected $table = 'users';
+
+    public function getAllUsers() {
+        return $this->getAll($this->table);
+    }
+}
