9ec083b6ea006cea83e90c1b53cd6084e85a97e7
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:50:38 +0200

    users

--- users.php
+++ users.php
@@ -1,1 +1,16 @@
 <?php
+
+require 'classes/user-view.php';
+require 'classes/user-model.php';
+
+$pdo = require 'partials/connect.php';
+
+$userModel = new UserModel($pdo);
+$userView = new UserView();
+
+include 'partials/header.php'; 
+include 'partials/nav.php';
+
+$userView->renderAllUsers($userModel->getAllUsers());
+
+include 'partials/footer.php';
