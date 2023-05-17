4caaf52c0f415969811676d277af347e64a73663
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:26:22 +0200

    user view

--- classes/user-view.php
+++ classes/user-view.php
@@ -1,1 +1,12 @@
 <?php
+
+class UserView {
+
+    public function renderAllUsers(array $users): void {
+        echo "<ul>";
+        foreach ($users as $user) {
+            echo "<li>{$user['name']} ({$user['id']})</li>";
+        }
+        echo "</ul>";
+    }
+}
