11a5131f2fc0861b1e0987aee0aff99cff2c9ea7
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:24:35 +0200

    book class

--- classes/book.php
+++ classes/book.php
@@ -1,1 +1,26 @@
 <?php
+
+class Book {
+
+    private string $title;
+    private int $year;
+    private int $authorId;
+
+    public function __construct(string $title, int $year, int $authorId) {
+        $this->title = $title;
+        $this->year = $year;
+        $this->authorId = $authorId;
+    }
+
+    public function getTitle(): string {
+        return $this->title;
+    }
+
+    public function getYear(): int {
+        return $this->year;
+    }
+
+    public function getAuthorId(): int {
+        return $this->authorId;
+    }
+}
