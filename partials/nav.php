116c2de1299a16f0d077afde081f9f92831fd734
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:33:21 +0200

    nav

--- partials/nav.php
+++ partials/nav.php
@@ -1,1 +1,8 @@
-<?php
+<nav>
+    <a href="index.php">Home</a>
+    <!-- <a href="index.php?page=book">Böcker</a> -->
+    <a href="users.php">Users</a>
+    <a href="books.php">Books</a>
+    <a href="authors.php">Authors</a>
+    <a href="reviews.php">Reviews</a>
+</nav>
