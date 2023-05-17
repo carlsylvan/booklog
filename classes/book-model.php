12216fc243870b179063ff8e21f431c9e5659321
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:21:56 +0200

    book model

--- classes/book-model.php
+++ classes/book-model.php
@@ -1,1 +1,18 @@
 <?php
+
+require_once 'db.php';
+
+class BookModel extends DB {
+
+    protected $table = 'books';
+
+    public function getAllBooks() {
+        return $this->getAll($this->table);
+    }
+
+    public function addBook(string $title, int $year, int $authorId) {
+        $sql = "INSERT INTO {$this->table} (title, year, author_id) VALUES (?, ?, ?)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$title, $year, $authorId]);
+    }
+}
