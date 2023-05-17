10f1ea64e9bb7ca0c15b50b617fefb4aa38bdfed
carlsylvan <carlsylvan@gmail.com>
2023-05-17 13:47:44 +0200

    index

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php   
 
+require 'classes/user-view.php';
+
+require 'classes/db.php';
+require 'classes/user-model.php';
+
+$pdo = require 'partials/connect.php';
+
+$db = new DB($pdo);
+$userModel = new UserModel($pdo);
+$userView = new UserView();
+
+include 'partials/header.php';
+include 'partials/nav.php';
+
+include 'partials/footer.php';
