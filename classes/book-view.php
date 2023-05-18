9c536ee735ebd021819a7b17cbab989152cd3e52
carlsylvan <carlsylvan@gmail.com>
2023-05-18 15:07:15 +0200

    book model

--- classes/book-view.php
+++ classes/book-view.php
@@ -9,4 +9,14 @@ class BookView {
         }
         echo "</ul>";
     }
+
+    public function renderAllBooksWithAuthors(array $books): void {
+        echo "<ul>";
+        foreach ($books as $book) {
+            echo "<li>{$book['title']} av {$book['first_name"]} {$book['last_name']} ({$book['year']})</li>";
+
+        }
+        echo "</ul>";
+    }
+    
 }
